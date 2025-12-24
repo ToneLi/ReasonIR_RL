8b02f85933ce9f5ba4e751f9da8d108e6bbb6320
ToneLi <33856565+ToneLi@users.noreply.github.com>
2025-12-24 11:00:02 -0500

    Update paths and cache directory in retriever script

--- 2_Retriever/retriever_script.sh
+++ 2_Retriever/retriever_script.sh
@@ -35,13 +35,13 @@ models=(diver-retriever)
 REASONING="30B_LLM_part${PART}_round${ROUND}"
 BS=-1
 
-path="/mnt/data_218/home1/Cool_Chen/0_reasonIR_bright/data/BRIGHT/"
-examples_path="/mnt/data_218/home1/Cool_Chen/0_reasonIR_bright/data_making/split_datasets/part_${PART}/"
-generated_files="/mnt/data_218/home1/Cool_Chen/0_reasonIR_bright/0_evaluation_upload/30B_LLM_dynamic_8_rounds_oupput_part${PART}.jsonl"    
+path="../0_reasoning_step_generation/data/BRIGHT/"
+examples_path="../0_reasoning_step_generation/data_making/split_datasets/part_${PART}/"
+generated_files="1_30B_output_organize/30B_LLM_dynamic_8_rounds_oupput_part${PART}.jsonl"    
 
 output_dir="output/part_${PART}_${MODEL}_reasoning_round${ROUND}"
-dataset_source="xlangai/BRIGHT"
-
+dataset_source="../0_reasoning_step_generation/data/BRIGHT/"
+cache_dir="../0_reasoning_step_generation/cache/cache_diver-retriever"
 tasks=(biology earth_science economics psychology robotics stackoverflow sustainable_living leetcode pony aops theoremqa_theorems)
 
 echo "=========================================="
@@ -51,7 +51,7 @@ echo "=========================================="
 for MODEL in ${models[@]}; do
     for TASK in ${tasks[@]}; do
         echo "Running task: $TASK (Round=$ROUND)"
-        CUDA_VISIBLE_DEVICES=$GPU python main_run.py --round_number $ROUND --path $path --examples_path $examples_path --generated_files $generated_files --task $TASK --model $MODEL --dataset_source ${dataset_source} --output_dir ${output_dir}  --cache_dir ${dataset_source}/cache/cache_${MODEL} --reasoning $REASONING --encode_batch_size $BS 
+        CUDA_VISIBLE_DEVICES=$GPU python main_run.py --round_number $ROUND --path $path --examples_path $examples_path --generated_files $generated_files --task $TASK --model $MODEL --dataset_source ${dataset_source} --output_dir ${output_dir}  --cache_dir ${cache_dir} --reasoning $REASONING --encode_batch_size $BS 
     done
 done
 
