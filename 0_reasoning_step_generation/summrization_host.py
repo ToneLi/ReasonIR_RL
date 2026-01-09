7cb77fea09b5ca68a10a8239623318e3b65030ed
ToneLi <33856565+ToneLi@users.noreply.github.com>
2026-01-09 12:27:13 -0500

    Refactor import statements and code structure

--- 0_reasoning_step_generation/summrization_host.py
+++ 0_reasoning_step_generation/summrization_host.py
@@ -8,6 +8,7 @@ app = FastAPI()
 
 generation_model="Qwen/Qwen3-30B-A3B-Instruct-2507"
 #generation_model="/data/home_beta/mingchen/3_DeepRetrieval/cold_start/output_dir/qwen3b_merged"
+#"/home/mingchen/3_Query_rewrite_RL/3_Diver-main/0_model_train/sft_output_4b_Thinking_pos1/final"
 # search_api = VectorSearchInterface(args, doc_ids, documents)
 
 # generation model
@@ -27,6 +28,7 @@ def summrization(req: dict):
     ]
 
     response_list =  gen_fn(messages_batch, max_tokens=8192, temperature=1, top_p=1,top_k=-1, n=1) # 32768
+
 
 
     return {"response_list":response_list}
