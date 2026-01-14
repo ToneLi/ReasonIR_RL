e0b67f0239964d7edddb3c9ed6bc62ea12d7ee58
ToneLi <33856565+ToneLi@users.noreply.github.com>
2026-01-14 17:31:11 -0500

    Refactor generation model assignments in summrization_host

--- 0_reasoning_step_generation/summrization_host.py
+++ 0_reasoning_step_generation/summrization_host.py
@@ -7,10 +7,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 generation_model="Qwen/Qwen3-30B-A3B-Instruct-2507"
-Qwen/Qwen3-30B-A3B-Instruct-2507   
-Qwen/Qwen3-4B-Thinking-2507
-deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
-deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+generation_model="Qwen/Qwen3-30B-A3B-Instruct-2507"  
+generation_model="Qwen/Qwen3-4B-Thinking-2507"  
+generation_model="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"  
+generation_model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"  
 
 #generation_model="/data/home_beta/mingchen/3_DeepRetrieval/cold_start/output_dir/qwen3b_merged"
 #"/home/mingchen/3_Query_rewrite_RL/3_Diver-main/0_model_train/sft_output_4b_Thinking_pos1/final"
@@ -40,5 +40,6 @@ def summrization(req: dict):
 
 
     return {"response_list":response_list}
+
 
 
